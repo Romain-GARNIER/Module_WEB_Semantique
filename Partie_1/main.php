6849c1b599e6079de1954cf28a8c76000eb8b8b1
Romain garnier <garnierr10@gmail.com>
2017-01-07 18:01:19 +0100

    ajout de logout et de main

---
+++ Partie_1/main.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pictionnary</title>
+    <link rel="stylesheet" media="screen" href="css/styles.css" >
+  </head>
+  <body>
+
+<?php require 'header.php'; ?>
+
+<div class="">
+  <a href="paint.php">Commencer à dessiner</a>
+</div>
+
+<div>
+<?php
+	if(isset($_SESSION['email']))
+	{
+    require 'connectionPDO.php';
+  	try
+  	{
+
+  		$sql = $dbh->prepare("SELECT d_did FROM drawings WHERE d_fk_u_id= :id");
+  		$sql->bindValue(":id", $_SESSION['id']);
+  		$sql->execute();
+  		$i = 0;
+      $res = $sql->fetchAll();
+  		foreach ($res as $ligne)
+  		{
+  			echo "<a href=guess.php?id=" . $ligne['d_id'] . ">Pictionnary " . ++$i . "</a><br/>";
+  		}
+  	}
+  	catch (PDOException $e)
+  	{
+  		print "<div class='PDOerreur'> Erreur !: " . $e->getMessage() . "</div>";
+  		$dbh = null;
+  		die();
+  	}
+	}
+?>
+</div>
+
+  </body>
+</html>
